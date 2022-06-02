9cdb46437eae8159bfe78cd8812131364dde1355
LuisGtk <luisgonzalez189@gmail.com>
2022-06-02 17:38:49 -0400

    finishing header,installed react

--- bitcoin-prices/src/App.js
+++ bitcoin-prices/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* creating nav tag */}
+      <nav>
+        {/* Link to our homepage */}
+        <h1>Global Bitcoin Prices</h1>
+        {/* adding logo for header */}
+        <img src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png" alt="Bitcoint logo"></img>
+      </nav>
     </div>
   );
 }
