432bfdf8d3317fb19fb00a4f0126547de40323ac
LuisGtk <luisgonzalez189@gmail.com>
2022-06-03 16:15:52 -0400

    starting useState, useEffect

--- bitcoin-prices/src/components/Currencies/Price/Price.js
+++ bitcoin-prices/src/components/Currencies/Price/Price.js
@@ -1,5 +1,21 @@
 import { useState, useEffect } from "react";
 
-export default function Price(props) {
-    console.log(props)
+export default function Price() {
+// beginning the use of useState
+    const [rate, setRate] = useState('')
+    const { currency } = props.match.params
+//creating a function to get data from the coindesk API based off country code we recieve
+const getApiData = async () => {
+    //API variable so when clicked, regardless of type of money, it will show the data
+    const apiEndPoint = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`
+    try {
+        //fetch
+        const response = await fetch(apiEndPoint);
+        const data = await response.json();
+        setRate(data.bpi[currency].rate)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 }
