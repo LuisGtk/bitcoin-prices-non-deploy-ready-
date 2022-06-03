baa088560149e5690793ff87c50703435eaceb10
LuisGtk <luisgonzalez189@gmail.com>
2022-06-03 15:34:45 -0400

    finishing home comp / starting currency list

--- bitcoin-prices/src/components/Home.js
+++ bitcoin-prices/src/components/Home.js
@@ -1,9 +1,10 @@
 import App from "../App.css"
+
 export default function Home() {
     return (
         <div>
-            <h1>This is the Homepage</h1>
-            <p>Welcome to my Bitcoin app made in React.</p>
+            <h1> Welcome the Homepage</h1>
+            <p>Welcome to my Bitcoin app made in React. In this application you will Click on a country and it will display how much 1 Bitcoin is currently at.</p>
         </div>
     )
 }
