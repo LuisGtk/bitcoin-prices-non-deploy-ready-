7f268e3b2b29fd1843037bfa98cd3ce78f88091c
LuisGtk <luisgonzalez189@gmail.com>
2022-06-08 16:07:32 -0400

    debugged for a while and installed axios

--- bitcoin-prices/src/App.js
+++ bitcoin-prices/src/App.js
@@ -1,13 +1,11 @@
-import { Routes, Route, Link, Redirect } from 'react-router-dom';
-import Home from './components/Currencies/Home';
-import Currencies from './components/Currencies/Currencies';
-import Price from './components/Currencies/Price/Price';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Currencies from './components/Currencies/Currencies/Currencies';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
       {/* creating nav tag */}
       <nav>
         {/* Link to our homepage */}
@@ -16,12 +14,10 @@ function App() {
           {/* adding logo for header */}
           <img src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png" alt="Bitcoinlogo"></img>
         </Link>
-
         <Link to='/Currencies'>
           <h1>Currencies</h1>
         </Link>
       </nav>
-
       {/* the main tag contains the paths the applicatuion takes you to after clicking one of the many links */}
       <main>
         {/* container for the route elements */}
@@ -29,7 +25,6 @@ function App() {
           {/* route path specifies which component a link will take you */}
           <Route path='/' element={<Home />} />
           <Route path="/Currencies" element={<Currencies />} />
-          <Route path="/Price" element={<Price />} />
         </Routes>
       </main>
     </div>
--- bitcoin-prices/src/components/Currencies/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1> Welcome the Homepage</h1>
-            <p>Welcome to my Crypto app made in React. While I wanted to reacreate an older Project the API I used before is no longer working and I've yet to find one with the info needed. I hope you can enjoy the CSS put into making this creative layout. I will be grabbing the price of a Crypto Cureencies in USD for now.</p>
-        </div>
-    )
-}
